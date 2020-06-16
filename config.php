5ee7efe2533d817fc5db0d9e311ba5aaf69276d7
Casweeney <casweeno2000@gmail.com>
2020-06-16 09:14:42 +0100

    Initial Commit

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+	define('CONSUMER_KEY', 'bhWmaYBesbHgT7mCyC0wazaCG');
+	define('CONSUMER_SECRET', 'iqnF9A66X5Hrj0Yx5s2DBDvylz4OEiPTLIme6NRACO5Lbr8FH0');
+	define('OAUTH_CALLBACK', 'http://localhost/login_with_twitter');
